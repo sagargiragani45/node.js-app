1c2605b25a6fd9e108a283e1cb590a6e8f0db582
sagargiragani45 <sagargoud2606@gmail.com>
2025-11-21 09:56:29 +0530

    initial commits

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node CI/CD Pipeline!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
